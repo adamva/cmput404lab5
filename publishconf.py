f9e379e87ccc14545329e30eb6b15824b09a70ec
avahmed <avahmed@ualberta.ca>
2023-02-06 16:09:20 -0700

    update site to use github.io pages

--- publishconf.py
+++ publishconf.py
@@ -7,11 +7,11 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://adamva.github.io/cmput404lab5'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml' # commented out see https://github.com/getpelican/pelican/issues/2489
 
 DELETE_OUTPUT_DIRECTORY = True
 
